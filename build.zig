51ddab4ac8fcca3a3c3c0c5899720ab0bac313b1
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2025-02-03 14:49:02 +0100

    Rename executable

--- build.zig
+++ build.zig
@@ -16,7 +16,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
 
     const exe = b.addExecutable(.{
-        .name = "dplinkr",
+        .name = "dplnkr",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
